a5d85103155c344dbc8bc5bc135a8fab15d1fb77
Brian Russell <Brianrussell2017@yahoo.com>
2024-11-25 16:42:08 -0500

    Add project files.

---
+++ Beverage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_4_WPF_Coffee
+{
+    public class Beverage
+    {
+        public double SizeInOunces { get; set; }
+
+        public string BeverageName { get; set; }
+
+        public double BeveragePrice { get; set; }
+    }
+}
---
+++ Coffee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3_4_WPF_Coffee
+{
+    public class Coffee : Beverage
+    {
+        public enum Roast
+        {
+            Light,
+            Medium,
+            Dark
+
+        }
+        public enum Temperature { Hot, Iced }
+        public string BrewStrength { get; set; }
+        public Roast RoastType { get; set; }
+        public Temperature CoffeeTemperature { get; set; }
+
+
+        public static List<Coffee> AddCoffee(Coffee coffee)
+        {
+            List<Coffee> Coffees = new List<Coffee>();
+            {
+                DataSource.Coffees.Add(coffee);
+
+            }
+
+            return Coffees;
+        }
+
+        public static List<Coffee> RemoveCoffee(Coffee coffee)
+        {
+            List<Coffee> Coffees = new List<Coffee>();
+            {
+                DataSource.Coffees.Remove(coffee);
+
+            }
+
+            return Coffees;
+        }
+
+    }
+}
